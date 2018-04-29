d590dddfcce8ec198d01905b24b2a42de15ab5b5
Unknown <aakop14@freeuni.edu.ge>
2018-04-29 15:21:54 +0400

    Completed commercials block
    Completed commercials block, added styles and images

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
-var commercialsHeight = 1200;
+var commercialsHeight = $( '#CommercialsWrapper' ).height();
 var alreadyFixed = false;
 var body = document.body,
     html = document.documentElement;
 
 function fixBlock(){
   var height = Math.max( body.scrollHeight, body.offsetHeight, 
-    html.clientHeight, html.offsetHeight );
+    html.clientHeight, html.offsetHeight ) - 53;
   var scrollSize = Math.max(window.pageYOffset, document.documentElement.scrollTop, window.scrollY);
 
-  if(!alreadyFixed && scrollSize >= (commercialsHeight - height)){
+  if(commercialsHeight > height){
+    if(!alreadyFixed && scrollSize >= (commercialsHeight - height)){
+      $('.commercials-wrapper').addClass("fix-commercials-wrapper");
+      alreadyFixed = true;
+    }else if(alreadyFixed && scrollSize < (commercialsHeight - height)){
+      $('.commercials-wrapper').removeClass("fix-commercials-wrapper");
+      alreadyFixed = false;
+    }
+  }else if(!alreadyFixed){
     $('.commercials-wrapper').addClass("fix-commercials-wrapper");
     alreadyFixed = true;
-  }else if(alreadyFixed && scrollSize < (commercialsHeight - height)){
-    $('.commercials-wrapper').removeClass("fix-commercials-wrapper");
-    alreadyFixed = false;
   }
 }
